a6bc286b2c7cc234c61cb22f399e12974b1edce5
Magali Espinosa <magalizethe@gmail.com>
2025-11-29 10:38:47 -0600

    Primer

---
+++ src/Banco.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Banco {
+    public static void main (String [] args){
+        String nombre = "Tony Stark";
+        String tipoCuenta= "Premium";
+        Double saldo = 1599.99;
+
+        System.out.println("**************");
+        System.out.println("\n Bienvenid@");
+        System.out.println("Nombre del cliente: " +nombre);
+        System.out.println("Tipo de cuenta: "+tipoCuenta);
+        System.out.println("Saldo disponible: $ "+saldo);
+        System.out.println("**************");
+
+
+
+    }
+}
+
